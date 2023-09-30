f9ebb88b657e26e556fc5b3c7c068df1ca53529b
rddaps <dapa0002@umn.edu>
2023-09-30 16:35:07 -0500

    processing city search

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,46 @@
-const { response } = require("express");
-
-var searchForm = $("searchForm");
-var searchInput = $("searchInput");
+var searchForm = document.getElementById("searchForm");
+var searchInput;
 var searchHistory = $("searchHistory");
 var weatherResults = $("weatherResults");
+var apiKey = "179a5a9fddd258890055139a6680139c";
 
-var city = $("city");
-var state = $("state");
-var country = $("country");
+var recentExplorations = JSON.parse(localStorage.getItem("recentExplorations"));
 
 function processSearch(event) {
   event.preventDefault();
 
-  searchInput = document.querySelector("#searchInput").value;
-}
+  searchInput = document.querySelector("searchInput").value;
 
-searchForm.addEventListener("submit", function (e) {});
+  localStorage.setItem("recentexplorations", JSON.stringify(recentExplorations));
 
-searchForm.addEventListener("submit", processSearch);
-
-var searchInput = document.querySelector("searchInput").ariaValueMax;
-
-function getWeather(city) {
-  fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
-  );
-}
-var APIKey = "###";
-
-var getCityCoordinates = function (city) {
-  var coordinatesUrl =
-    "https://api.openweathermap.org/geo/1.0/direct?q=" +
-    city +
-    "&limit=2&appid=" +
-    apiKey;
-
-  fetch(coordinatesUrl)
-    .then(function (response) {
-      if (response.ok) {
-        console.log(response);
-        response.json().then(function (data) {
-          console.log(data);
-          displayCordinates(data, city);
-        });
-      } else {
-        alert("Error: " + response.statusText);
-      }
+    var coordinatesUrl =
+      `https://api.openweathermap.org/geo/1.0/direct?q=${`searchInput`}&limit=1&appid=${`apiKey`}`;
+  
+    fetch(coordinatesUrl)
+      .then(function (response) {
+        if (!response.ok) {
+            throw response.json();
+        }
+        return response.json();
     })
-    .catch(function (error) {
-      alert("Unable to identify city");
-    });
+          .then(function (data) {
+            var latitude = data[0].lat;
+            var longitutue = data[0].lon;
+
+            var weatherUrl =
+                `https://api.openweathermap.org/data/2.5/forecast?lat=${`latitude`}&lon=${`longitude`}&appid=${`apiKey`}`;
+            
+              fetch(weatherUrl)
+              .then(function (response) {
+                if (!response.ok) {
+                    throw response.json();
+                }
+                return response.json();
+                })
+                  .then(function (data) {
+                    displayWeather(data);
+                  });
+            })
 };
 
-// function convertCityData(city) {
-//     fetch('https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=2&appid=${apiKey}')
-//     .then(response => response.json())
-//     .then
-// }
+
