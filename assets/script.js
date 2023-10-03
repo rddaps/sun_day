039d26ba7b39b63bbcf57f146366b663644b8fc9
rddaps <dapa0002@umn.edu>
2023-10-03 03:15:18 -0500

    displaying forecast weather data

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,28 @@
 var searchForm = document.getElementById("searchForm");
-var searchInput;
+var searchInput = document.getElementById("searchInput");
 var searchHistory = [];
 var weatherResults = $("#weatherResults");
 var apiKey = "179a5a9fddd258890055139a6680139c";
 
-var recentExplorations = JSON.parse(localStorage.getItem(["searchHistory"]));
-localStorage.setItem("recentExplorations", JSON.stringify(searchHistory));
+var recentExplorations =
+  JSON.parse(localStorage.getItem("recentExplorations")) || [];
 
 function processSearch(event) {
   event.preventDefault();
+  var searchInputValue = searchInput.value.trim();
 
-  searchInput = document.getElementById("searchInput").value;
-  saveHistory(searchInput);
+  if (!searchInputValue) {
+    return;
+  }
+
+  saveHistory(searchInputValue);
 
   var coordinatesUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=1&appid=${apiKey}`;
 
   fetch(coordinatesUrl)
     .then(function (response) {
       if (!response.ok) {
-        throw response.json();
+        throw new Error("City not found");
       }
       return response.json();
     })
@@ -29,65 +33,73 @@ function processSearch(event) {
       var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
       var futureWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
-      fetch(currentWeatherUrl)
-        .then(function (response) {
-          if (!response.ok) {
-            throw response.json();
-          }
-          return response.json();
+      Promise.all([fetch(currentWeatherUrl), fetch(futureWeatherUrl)])
+        .then(function (responses) {
+          return Promise.all(
+            responses.map(function (response) {
+              if (!response.ok) {
+                throw new Error("Weather data not available");
+              }
+              return response.json();
+            })
+          );
         })
-        .then(function (currentWeatherData) {
-          displayWeather(currentWeatherData);
-        });
+        .then(function (results) {
+          var currentWeatherData = results[0];
+          var futureWeatherData = results[1].list;
 
-      fetch(futureWeatherUrl)
-        .then(function (response) {
-          if (!response.ok) {
-            throw response.json();
-          }
-          return response.json();
+          displayWeather(currentWeatherData, futureWeatherData);
         })
-        .then(function (response) {
-          var forecastData = response.list;
+        .catch(function (error) {
+          console.error(error);
         });
-        
+    })
+    .catch(function (error) {
+      console.error(error);
     });
 }
 
 function saveHistory(searchInput) {
-  if (searchHistory > 5) {
-    searchHistory.pop();
-    var recentExplorations = `<li>${searchInput}</li>`;
-    $("#searchHistory").append(recentExplorations);
+  if (searchHistory.length >= 5) {
+    searchHistory.shift();
   }
+  searchHistory.push(searchInput);
+  localStorage.setItem("recentExplorations", JSON.stringify(searchHistory));
 
-  $("#searchHistory").on("click", "li", function () {
-    var searchInput = $(this).text();
-    displayWeather(searchInput);
+  displaySearchHistory();
+}
+
+function displaySearchHistory() {
+  var historyList = $("#searchHistory");
+  historyList.empty();
+  searchHistory.forEach(function (searchItem) {
+    historyList.prepend(`<li>${searchItem}</li>`);
   });
 }
 
-function displayWeather(weatherResults) {
-  console.log(weatherResults);
-  $(`#currentCity`)
-    .text(
-      searchInput + ", " + moment.unix(weatherResults.dt).format("MM/DD/YYYY")
-    )
+function displayWeather(currentWeatherData, futureWeatherData) {
+  console.log(currentWeatherData, futureWeatherData);
+  var currentCity = ${searchInput.value}, ${moment.unix(currentWeatherData.dt).format("MM/DD/YYYY")};
     .append(
       '<img src="http://openweathermap.org/img/wn/' +
-        weatherResults.weather[0].icon +
+        currentWeatherData.weather[0].icon +
         '.png"/>'
     );
 
-  $(`#currentTemp`).text("Temp: " + weatherResults.main.temp + "°C");
-  $(`#currentWind`).text("Wind Speed: " + weatherResults.wind.speed + "m/s");
-  $(`#currentHumidity`).text("Humidity: " + weatherResults.main.humidity + "%");
+    var currentTemp = Temp: ${currentWeatherData.main.temp}°C;
+    var currentWind = Wind Speed: ${currentWeatherData.wind.speed}m/s;
+    var currentHumidity = Humidity: ${currentWeatherData.main.humidity}%;
+    $("#currentCity").text(currentCity);
+    $("#currentTemp").text(currentTemp);
+    $("#currentWind").text(currentWind);
+    $("#currentHumidity").text(currentHumidity);
 
-  for (var i = 1; i < forecastData.length; i+= 8) {
-    var forecastDate = forecastData[i].dt_txt;
-    var temperature = forecastData[i].main.temp;
-    var humidity = forecastData[i].main.humidity;
-    var windSpeed = forecastData[i].wind.speed;
+  $("#weatherResults").empty();
+  for (var i = 0; i < futureWeatherData.length; i += 8) {
+    var forecastDate = futureWeatherData[i].dt_txt;
+    var temperature = futureWeatherData[i].main.temp;
+    var humidity = futureWeatherData[i].main.humidity;
+    var windSpeed = futureWeatherData[i].wind.speed;
 
     var forecastItem = `<div class="forecast-item">
                             <h3>${forecastDate}</h3>
@@ -96,8 +108,9 @@ function displayWeather(weatherResults) {
                             <p>Wind Speed: ${windSpeed} m/s</p>
                           </div>`;
 
-    $("#weather-info").append(forecastItem);
+    $("#weatherResults").append(forecastItem);
   }
 }
 
 searchForm.addEventListener("submit", processSearch);
+displaySearchHistory();
