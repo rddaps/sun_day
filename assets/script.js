6e311dec1c42d08c6db393679e666d0d4e21b3c0
rddaps <dapa0002@umn.edu>
2023-10-03 00:11:14 -0500

    current data set

--- assets/script.js
+++ assets/script.js
@@ -1,97 +1,103 @@
 var searchForm = document.getElementById("searchForm");
 var searchInput;
-var searchHistory = $("#searchHistory");
+var searchHistory = [];
 var weatherResults = $("#weatherResults");
 var apiKey = "179a5a9fddd258890055139a6680139c";
 
-var recentExplorations = JSON.parse(localStorage.getItem("recentExplorations")) || [];
+var recentExplorations = JSON.parse(localStorage.getItem(["searchHistory"]));
+localStorage.setItem("recentExplorations", JSON.stringify(searchHistory));
 
 function processSearch(event) {
   event.preventDefault();
 
   searchInput = document.getElementById("searchInput").value;
+  saveHistory(searchInput);
 
-  localStorage.setItem("recentexplorations", JSON.stringify(recentExplorations));
+  var coordinatesUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=1&appid=${apiKey}`;
 
-    var coordinatesUrl =
-      `https://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=1&appid=${apiKey}`;
-  
-    fetch(coordinatesUrl)
-      .then(function (response) {
-        if (!response.ok) {
-            throw response.json();
-        }
-        return response.json();
+  fetch(coordinatesUrl)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
     })
-          .then(function (currentWeatherData) {
-            currentWeather(currentWeatherData);
-            var latitude = currentWeatherData[0].lat;
-            var longitude = currentWeatherData[0].lon;
+    .then(function (data) {
+      var latitude = data[0].lat;
+      var longitude = data[0].lon;
 
-            var weatherUrl =
-                `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-            
-              fetch(weatherUrl)
-              .then(function (response) {
-                if (!response.ok) {
-                    throw response.json();
-                }
-                return response.json();
-                })
-                  .then(function (futureWeatherData) {
-                    displayWeather(currentWeatherData);
-                    displayWeather(futureWeatherData);
-                  });
-            })
-};
+      var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+      var futureWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
-function saveHistory() {
-    if(recentExplorations.length > 5) {
-        recentExplorations.pop();
-    };
+      fetch(currentWeatherUrl)
+        .then(function (response) {
+          if (!response.ok) {
+            throw response.json();
+          }
+          return response.json();
+        })
+        .then(function (currentWeatherData) {
+          displayWeather(currentWeatherData);
+        });
 
-    for(i = 0; i < recentExplorations.length; i++) {
-        $(`#searchHistory`).append(recentExplorations[i]);
-    }
-};
-
-function displayWeather(weatherResults) {
-    $(`#currentCity`)
-        .text(searchInput + ", " + moment.unix(weatherResults.date).format("MM/DD/YYYY"))
-        .append('<img src="http://openweathermap.org/img/wn/'+ weatherResults.current.weather[0].icon +'.png"/>');
-
-        $(`#currentTemp`).text("Temp: " + weatherResults.current.temp);
-        $(`#currentWind`).text("Wind Speed: " + weatherResults.current.windSpeed);
-        $(`#currentHumidity`).text("Humidity: " + weatherResults.current.humidity + "%");
-
-        for(i=0; i<5; i++) {
-
-        var fiveDayOutlook = document.createElement("div")
-        fiveDayOutlook.classList = "card col-5 mb-3 bg-primary text-light";
-        $('#5DayOutlook').append(fiveDayOutlook);
-
-        var fiveDayOutlookDate = moment.unix(weatherResults.daily[i].dt).format("MM/DD/YYYY")
-        var eachDayEl = document.createElement("p");
-        fiveDayOutlook.appendChild(eachDayEl).textContent = fiveDayOutlookDate;
-
-        var fiveDayOutlookIcon = document.createElement("img");
-        fiveDayOutlookIcon.setAttribute("src", "http://openweathermap.org/img/wn/"+ weatherResults.daily[i].weather[0].icon +".png")
-        fiveDayOutlookIcon.classList = "w-25";
-        fiveDayOutlook.appendChild(fiveDayOutlookIcon);
-
-        var fiveDayOutlookTemp = weatherResults.daily[i].temp.day;
-        var eachDayTempEl = document.createElement("p");
-        fiveDayOutlook.appendChild(eachDayTempEl).textContent = 'Temp: ' + fiveDayOutlookTemp;
-
-        var fiveDayOutlookWind = weatherResults.daily[i].wind_speed;
-        var eachDayWindEl = document.createElement("p");
-        fiveDayOutlook.appendChild(eachDayWindEl).textContent = 'Wind Speed: ' + fiveDayOutlookWind;
-
-        var fiveDayOutlookHumidity = weatherResults.daily[i].humidity;
-        var eachDayHumidity = document.createElement("p");
-        fiveDayOutlook.appendChild(eachDayHumidity).textContent = 'Humidity: ' + fiveDayOutlookHumidity;
-
-        }
+      fetch(futureWeatherUrl)
+        .then(function (response) {
+          if (!response.ok) {
+            throw response.json();
+          }
+          return response.json();
+        })
+        .then(function (response) {
+          var forecastData = response.list;
+        });
+        
+    });
 }
 
-searchForm.addEventListener('submit', processSearch)
+function saveHistory(searchInput) {
+  if (searchHistory > 5) {
+    searchHistory.pop();
+    var recentExplorations = `<li>${searchInput}</li>`;
+    $("#searchHistory").append(recentExplorations);
+  }
+
+  $("#searchHistory").on("click", "li", function () {
+    var searchInput = $(this).text();
+    displayWeather(searchInput);
+  });
+}
+
+function displayWeather(weatherResults) {
+  console.log(weatherResults);
+  $(`#currentCity`)
+    .text(
+      searchInput + ", " + moment.unix(weatherResults.dt).format("MM/DD/YYYY")
+    )
+    .append(
+      '<img src="http://openweathermap.org/img/wn/' +
+        weatherResults.weather[0].icon +
+        '.png"/>'
+    );
+
+  $(`#currentTemp`).text("Temp: " + weatherResults.main.temp + "°C");
+  $(`#currentWind`).text("Wind Speed: " + weatherResults.wind.speed + "m/s");
+  $(`#currentHumidity`).text("Humidity: " + weatherResults.main.humidity + "%");
+
+  for (var i = 1; i < forecastData.length; i+= 8) {
+    var forecastDate = forecastData[i].dt_txt;
+    var temperature = forecastData[i].main.temp;
+    var humidity = forecastData[i].main.humidity;
+    var windSpeed = forecastData[i].wind.speed;
+
+    var forecastItem = `<div class="forecast-item">
+                            <h3>${forecastDate}</h3>
+                            <p>Temperature: ${temperature}°C</p>
+                            <p>Humidity: ${humidity}%</p>
+                            <p>Wind Speed: ${windSpeed} m/s</p>
+                          </div>`;
+
+    $("#weather-info").append(forecastItem);
+  }
+}
+
+searchForm.addEventListener("submit", processSearch);
